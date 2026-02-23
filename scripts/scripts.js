73bf26deb0fa87a91f190b8238ec24894f2b00bc
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2026-02-23 22:55:01 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -137,6 +137,17 @@ function loadDelayed() {
   alert('hi')
   // eslint-disable-next-line import/no-cycle
   window.setTimeout(() => import('./delayed.js'), 3000);
+  // add delayed functionality here
+    console.log("DOM fully loaded and parsed");
+let blocks =document.querySelectorAll('[data-block-name]');
+  if(blocks.length > 0){
+    const json = {};
+    for(var i=0; i< blocks.length;i++ )
+    let blockName = blocks[i].getAttribute('blockName');
+  }
+
+   
+
   // load anything that can be postponed to the latest here
 }
 
