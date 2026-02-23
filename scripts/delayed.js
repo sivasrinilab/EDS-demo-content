cf101147e3ad77b8e263f2313f0c1ba4488fedbd
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2026-02-23 15:01:32 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,7 @@
 // add delayed functionality here
+addEventListener("DOMContentLoaded", (event) => {
+
+
+   const json = {};
+ alert( document.querySelectorAll('div.section > [class^="-wrapper"] > div').textContent);
+})
