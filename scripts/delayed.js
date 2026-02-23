0c5f0d55b3f0e1122400708046835591ce812490
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2026-02-23 22:42:03 +0530

    Remove alert and DOMContentLoaded listener
    Removed alert for text content of specific divs and the DOMContentLoaded event listener.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,9 +1,5 @@
 // add delayed functionality here
- alert( document.querySelectorAll('div.section > [class^="-wrapper"] > div').textContent);
-document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
   
 
    const json = {};
- alert( document.querySelectorAll('div.section > [class^="-wrapper"] > div').textContent);
-})
