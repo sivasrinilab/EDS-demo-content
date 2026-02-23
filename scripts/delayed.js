883859ed99d8657018087398d90e170dc473d373
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2026-02-23 22:53:37 +0530

    Update delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,5 +1,10 @@
 // add delayed functionality here
     console.log("DOM fully loaded and parsed");
-  
+let blocks =document.querySelectorAll('[data-block-name]');
+  if(blocks.length > 0){
+    const json = {};
+    for(var i=0; i< blocks.length;i++ )
+    let blockName = blocks[i].getAttribute('blockName');
+  }
 
-   const json = {};
+   
