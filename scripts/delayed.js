351dbad29aa206d9669cf349620f362633f942a1
sivasrinilab <88289718+sivasrinilab@users.noreply.github.com>
2026-02-23 23:03:33 +0530

    Remove commented code from delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,10 +1,1 @@
-// add delayed functionality here
-    console.log("DOM fully loaded and parsed");
-let blocks =document.querySelectorAll('[data-block-name]');
-  if(blocks.length > 0){
-    const json = {};
-    for(var i=0; i< blocks.length;i++ )
-    let blockName = blocks[i].getAttribute('blockName');
-  }
 
-   
